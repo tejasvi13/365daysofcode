f783390b8d7f3e22a31f5144713ff55ff0721780
Tejasvi Jakkampudi <123182611+tejasvi13@users.noreply.github.com>
2023-08-11 23:03:25 +0530

    Add files via upload

---
+++ Coding/Day16/AddDigits.java
@@ -0,0 +1,15 @@
+//Question: https://leetcode.com/problems/add-digits/description/
+class Solution {
+    public int addDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum+=num % 10;
+            num=num / 10;
+           if (num == 0 && sum> 9) {
+                num = sum;
+                sum= 0;  
+            }    
+        }     
+        return sum;
+    }
+}
