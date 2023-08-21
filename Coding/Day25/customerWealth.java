b91468bace28cf34375eba88310e308930b98a9a
Tejasvi Jakkampudi <123182611+tejasvi13@users.noreply.github.com>
2023-08-21 22:20:14 +0530

    Add files via upload

---
+++ Coding/Day25/customerWealth.java
@@ -0,0 +1,16 @@
+//Question: https://leetcode.com/problems/richest-customer-wealth/description/
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int sum=0;int max=0;
+        for(int i=0;i<accounts.length;i++){
+            for(int j=0;j<accounts[i].length;j++){
+                sum+=accounts[i][j];
+            }
+            if(sum>max){
+                max=sum;
+            }
+            sum=0;
+        }
+        return max;
+    }
+}
---
+++ Coding/Day25/repeatedStringPattern.java
@@ -0,0 +1,18 @@
+//Question: https://leetcode.com/problems/repeated-substring-pattern/description/
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n=s.length();
+        for(int i=1;i<=n/2;i++){
+            if(n%i==0){
+                StringBuilder p=new StringBuilder();
+                for(int j=0;j<n/i;j++){
+                    p.append(s.substring(0,i));
+                }
+                if(s.equals(p.toString())){
+                    return true;
+                }
+            }            
+        }
+        return false;
+    }
+}
